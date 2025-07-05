31eccda0c2c889dd41830a1b7639a986404743bd
SaisatwikBiku <bsaisatwik@yahoo.com>
2025-07-05 13:02:13 -0400

    handled deprecation warnings in the backend

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Atlas connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Atlas connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
